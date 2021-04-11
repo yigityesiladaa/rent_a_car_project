c80c802b72965bd4d98102e1d5d1659afdaf89f1
yigityesilada <yy.yesilada@gmail.com>
2021-04-11 18:53:47 +0300

    11.04.2021

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -7,6 +7,12 @@ namespace Business.Abstract
 {
 	public interface ICarService
 	{
+		public void Add(Car car);
+		public void Update(Car car);
+		public void Delete(Car car);
 		public List<Car> GetCars();
+		public List<Car> GetCarsByBrandID(int BrandID);
+		public List<Car> GetCarsByColorID(int ColorID);
+
 	}
 }
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,37 @@ namespace Business.Concrete
 			_iCarDal = iCarDal;
 		}
 
+		public void Add(Car car)
+		{
+			if(car.CarName.Length >= 2 && car.DailyPrice > 0)
+			{
+				_iCarDal.Add(car);
+			}
+		}
+
+		public void Delete(Car car)
+		{
+			_iCarDal.Delete(car);
+		}
+
 		public List<Car> GetCars()
 		{
-			return _iCarDal.GetCars();
+			return _iCarDal.GetAll();
+		}
+
+		public List<Car> GetCarsByBrandID(int BrandID)
+		{
+			return _iCarDal.GetAll(car=> car.BrandId == BrandID);
+		}
+
+		public List<Car> GetCarsByColorID(int ColorID)
+		{
+			return _iCarDal.GetAll(car => car.ColorId == ColorID);
+		}
+
+		public void Update(Car car)
+		{
+			_iCarDal.Update(car);
 		}
 	}
 }
